17c5f3b27f52cd160a94fbe5dbaa721cf8a62fc9
Fabien <froyer67@gmail.com>
2018-11-23 13:42:42 -0800

    remove reset between commands

--- weed/cli.lua
+++ weed/cli.lua
@@ -3,7 +3,7 @@ local config = require("config")
 
 local function send(arg)
   print("cmd: " .. arg[1])
-  local cfg = config.getConfig("./config/config.json")
+  local cfg = config.getConfig("./config/config.toml")
   local cli = 0
   local err = ""
   cli, err = socket.connect(cfg.shell.bind, cfg.shell.port)
@@ -11,17 +11,13 @@ local function send(arg)
     cli:send(arg[1] .. "\n")
     local resp = cli:receive("*a")
     if resp ~= nil then
-      print("resp: " .. resp)
+      print("response:\n" .. resp)
     else
-      print("no response")
+      print("no response\n")
     end
     cli:shutdown()
     cli:close()
   end
 end
-
-local t = {}
-t[1] = "reset"
-send(t)
 
 send(arg)
